1265e9bc4a6f026e889c7dcc0f96ec9f78bcf238
Daniil Hvozdz <93543909+flytrapq@users.noreply.github.com>
2021-11-22 02:40:12 +0200

    Update password-generator.cpp
    Commented some lines that may cause problems

--- password-generator.cpp
+++ password-generator.cpp
@@ -1,3 +1,7 @@
+// if the program works correctly (you don't see sth like " \033[0;32m ")
+// remove the " //* " comments in order to see sOmE cRaZy CoLoRs 
+
+
 #include <iostream>
 #include <random>
 #include <string>
@@ -25,16 +29,16 @@ void GenereatePass(char password[], int length) {
     for (int j = 0; j < length; j++) {
         mt19937 mersenne(rd());
         password[j] = possible_chars[mersenne() % 89];
-        cout << "\033[0;32m";
+        //* cout << "\033[0;32m";
         cout << password[j];
     }
-    cout << "\033[0;37m";
+    //* cout << "\033[0;37m";
     cout << " '\n\nFeel free to copy!\n\n";
 }
 
 int main()
 {
-    cout << "\033[0;37m";
+    //* cout << "\033[0;37m";
     bool Continue = true;
     while (Continue) {
         // we gotta check if the password is at least 1 character long
@@ -47,9 +51,9 @@ int main()
                 length = stoi(temp);
             }
             else {
-                cout << "\033[0;31m";
+                //* cout << "\033[0;31m";
                 cout << "The number is not recognised, try again.\n\n";
-                cout << "\033[0;37m";
+                //* cout << "\033[0;37m";
                 continue;
             }
 
